6bbbc903888548ba6ea5b571ae881dee7a717716
joker <zai1020733278@gmail.com>
2020-05-09 14:50:15 +0800

    fix

--- app/Manager/Game.php
+++ app/Manager/Game.php
@@ -32,27 +32,6 @@ class Game
         }
     }
 
-    public function printGameMap()
-    {
-        $map_data = $this->game_map->getMapData();
-        $font     = [2 => '追', 3 => '躲'];
-        foreach ($this->players as $player) {
-            $map_data[$player->getX()][$player->getY()] = $player->getType() + 1;
-        }
-        foreach ($map_data as $line) {
-            foreach ($line as $item) {
-                if (empty($item)) {
-                    echo "墙，";
-                } elseif ($item == 1) {
-                    echo "    ";
-                } else {
-                    echo $font[$item] . '，';
-                }
-            }
-            echo PHP_EOL;
-        }
-    }
-
     private function canMoveToDirection($player, $direction)
     {
         $x = $player->getX();
